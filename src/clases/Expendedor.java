d898fe603196764cc4112d6666c43118b6209edb
Roberto <pantostado1601@gmail.com>
2024-04-26 14:59:52 -0400

    add Expendedor version PAEP

--- src/clases/Expendedor.java
+++ src/clases/Expendedor.java
@@ -1,5 +1,62 @@
 package src.clases;
 
-public class Expendedor {
-    // Completar...
+class Expendedor {
+    private Deposito coca;
+    private Deposito sprite;
+    private DepositoM monVu;
+    private int precio;
+
+    public static final int COCA = 1;
+    public static final int SPRITE = 2;
+
+    public Expendedor(int numBebidas, int precioBebidas) {
+        coca = new Deposito();
+        sprite = new Deposito();
+        monVu = new DepositoM();
+
+        precio = precioBebidas;
+
+        for (int i = 0; i < numBebidas; i++) {
+            coca.addBebida(new CocaCola(100 + i));
+            sprite.addBebida(new Sprite(200 + i));
+        }
+    }
+
+    public Bebida comprarBebida(Moneda moneda, int tipo) {
+        if (moneda == null) {
+            return null;
+        }
+        if (moneda.getValor() < precio || (tipo != COCA && tipo != SPRITE)) {
+            monVu.addMoneda(moneda);
+            return null;
+        }
+        for (int i = moneda.getValor() - precio; i > 0; i -= 100) {
+            monVu.addMoneda(new Moneda100());
+        }
+        Bebida auxBebida = null;
+        switch (tipo) {
+            case COCA:
+                auxBebida = coca.getBebida();
+                if (auxBebida == null) {
+                    for (int i = 0; i < precio; i += 100) {
+                        monVu.addMoneda(new Moneda100());
+                    }
+                }
+                return auxBebida;
+            case SPRITE:
+                auxBebida = sprite.getBebida();
+                if (auxBebida == null) {
+                    for (int i = 0; i < precio; i = i + 100) {
+                        monVu.addMoneda(new Moneda100());
+                    }
+                }
+                return auxBebida;
+            default:
+                return null;
+        }
+    }
+
+    public Moneda getVuelto() {
+        return monVu.getMoneda();
+    }
 }
