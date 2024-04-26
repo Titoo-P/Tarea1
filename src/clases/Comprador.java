13422078fefb33326039f3088618c4a752a6a8d5
Roberto <pantostado1601@gmail.com>
2024-04-26 14:59:15 -0400

    Add Comprador version PAEP

--- src/clases/Comprador.java
+++ src/clases/Comprador.java
@@ -1,5 +1,33 @@
 package src.clases;
 
-public abstract class Comprador {
-    // Completar...
+class Comprador {
+    private String sonido;
+    private int vuelto;
+
+    public Comprador(Moneda m, int cualBebida, Expendedor exp) {
+        Bebida aux = exp.comprarBebida(m, cualBebida);
+
+        vuelto = 0;
+        do {
+            Moneda auxM = exp.getVuelto();
+            if (auxM == null) {
+                break;
+            }
+            vuelto += auxM.getValor();
+        } while (true);
+
+        if (aux != null) {
+            sonido = aux.beber();
+        } else {
+            sonido = null;
+        }
+    }
+
+    public int cuantoVuelto() {
+        return vuelto;
+    }
+
+    public String queBebiste() {
+        return sonido;
+    }
 }
